2e85434a81f467fcfafbfb48d4ca50b6888e2ecd
dani198 <danyleul16@gmail.com>
2023-03-06 09:30:12 +0300

    route

--- 0-hello_route.py
+++ 0-hello_route.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run()
